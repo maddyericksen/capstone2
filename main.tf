eb1f0199defae0cea40ecba8bdbbd8cad65aba98
maddyericksen <ericksen.madison@gmail.com>
2024-05-14 19:19:13 +0000

    Added Dockerfile

--- main.tf
+++ main.tf
@@ -16,3 +16,24 @@ terraform {
 provider "aws" {
   region = "us-west-2"
 }
+
+##
+## Create an S3 bucket to hold the application data.
+##
+resource "aws_s3_bucket" "todos_data_bucket" {
+  bucket  = "${var.group_alias}-data"
+  key     = "student.alias"
+  content = "This bucket is reserved for ${var.student_alias}"
+}
+
+# Creating a lambda function
+resource "aws_lambda_function" "grp3-cap2b-gettodos" {
+  triggers = {
+    
+  }
+}
+
+# API Gateway
+resource "aws_api_gateway_rest_api" "grp3-cap2b-api" {
+  # Lambda proxy integration = False
+}
